b1a3e065eb1362b7d1e404640ff7dca887750e67
Yu YeongH <julie5050.co@gmail.com>
2021-05-21 21:57:59 +0900

    player movement

---
+++ Assets/Script/moving.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moving : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W)) 
+            Debug.Log("위로 점프 중");
+        
+        if (Input.GetKeyDown(KeyCode.A)) 
+            Debug.Log("왼쪽으로 이동 중");
+        
+        if (Input.GetKeyDown(KeyCode.S)) 
+            Debug.Log("아래로 이동 중");
+
+        if (Input.GetKeyDown(KeyCode.D)) 
+            Debug.Log("오른쪽으로 이동 중");
+        
+        if (Input.GetMouseButton(0))
+            Debug.Log("공격!");
+    }
+}
---
+++ Assets/Script/transform.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class transform : MonoBehaviour
+{
+    //public Vector2 speed_vec;
+    //Behav behav;    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 우주하마 움직임 코드 1 -> 속도 조절을 어떻게 해야할지 모르겠음.
+       Vector3 vec1 = new Vector3(
+            Input.GetAxis("Horizontal"),
+            Input.GetAxis("Vertical"), 
+            0);
+       transform.Translate(vec1);
+
+       //speed_vec = Vector2.zero ; 
+
+       //if (Input.GetKeyDown(KeyCode.D)) 
+       // {
+       //     speed_vec.x += 0.1f ; 
+
+       // }
+       //     Debug.Log("위로 점프 중"); 
+       // transform.Translate(speed_vec) ; 
+    }
+}
