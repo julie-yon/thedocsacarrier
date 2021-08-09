6401f7beaf88b41c517d3f426a82d9e33feac06a
Shin SeockJu <margron@naver.com>
2021-08-09 15:34:02 +0900

    Dialogue System modified
    Scriptable Script has been diverged from ManagerScript

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -106,16 +106,4 @@ namespace Utility
             
         }
     }
-
-    [CreateAssetMenu(fileName ="Dialogue", menuName = "ScriptableObjects/Dialogue")]
-    [System.Serializable]
-    public class DialogueScriptable : ScriptableObject
-    {
-        public string Name;
-        public List<string> Message;
-        public DialogueScriptable NextDialogue;
-        public Sprite LeftSpeakerSprite;
-        public Sprite RightSpeakerSprite;
-
-    }
 }
---
+++ Assets/Scripts/DialogueScriptable.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Utility
+{
+    [CreateAssetMenu(fileName ="Dialogue", menuName = "ScriptableObjects/Dialogue")]
+    [System.Serializable]
+    public class DialogueScriptable : ScriptableObject
+    {
+        public string Name;
+        [TextArea(3, 10)]
+        public List<string> Message;
+        public DialogueScriptable NextDialogue;
+        public Sprite LeftSpeakerSprite;
+        public Sprite RightSpeakerSprite;
+
+    }
+}
