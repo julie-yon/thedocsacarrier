704615a04d128ea94dafa5f17c500d89a4d66f33
Shin SeockJu <margron@naver.com>
2021-09-30 19:26:37 +0900

    UniqueActions folder added
    UniqueActions folder contains some specific script for one actions
    Now it has only CaveEntrance

---
+++ Assets/Scripts/UniqueActions/CaveEntrance.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Docsa
+{
+    public class CaveEntrance : MonoBehaviour
+    {
+        public string CaveEntranceObjectName;
+
+        void Awake()
+        {
+            CaveEntranceObjectName = gameObject.name;
+        }
+
+        void OnTriggerEnter2D(Collider2D collider2D)
+        {
+            print("Trigger");
+            print(collider2D.gameObject.layer + collider2D.name);
+            print(LayerMask.GetMask("UzuHama"));
+            if (1 << collider2D.gameObject.layer == LayerMask.GetMask("UzuHama"))
+            {
+                print("CaveEntrance");
+            }
+        }
+    }
+}
