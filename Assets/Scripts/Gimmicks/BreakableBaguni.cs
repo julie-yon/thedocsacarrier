49f69df274b0d4f1ca35774232cb761d0e097141
Shin SeockJu <margron@naver.com>
2022-03-04 06:26:18 +0900

    Breakable Baguni adde

---
+++ Assets/Scripts/Gimmicks/BreakableBaguni.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Docsa.Gimmick
+{
+    public class BreakableBaguni : MonoBehaviour
+    {
+        public float HP = 300;
+        public LayerMask TargetLayerMask;
+        void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (((1 << collision.gameObject.layer) & TargetLayerMask.value) > 0)
+            {
+                GetDamage(collision.gameObject.GetComponent<Projectile>().DamageValue);
+            }
+        }
+
+        public void GetDamage(float damageValue)
+        {
+            HP -= damageValue;
+            if (HP <= 0) Break();
+        }
+
+        public void Break()
+        {
+            Destroy(gameObject);
+        }
+    }
+}
