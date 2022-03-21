696d2b7bded2581b921d970207a79c649bc9f21c
Shin SeockJu <margron@naver.com>
2022-03-21 10:27:59 +0900

    Character Die Animation event added

---
+++ Assets/Scripts/UniqueEvents/CharacterDieAnimationEvent.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+using Docsa.Character;
+
+namespace Docsa.Events
+{
+    public class CharacterDieAnimationEvent : MonoBehaviour
+    {
+        void Die()
+        {
+            transform.parent.GetComponent<Docsa.Character.Character>().Return();
+        }
+    }
+}
